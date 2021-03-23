c093011eabafaddc52426f80f3b494d8c96c45cf
Hariton <yourrasik@gmail.com>
2021-03-23 21:13:28 +1000

    Add: article model

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.include.FactoryBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
