966a25d85c57d59745e2950e9b9d9ae6b61ee4f7
Hariton <yourrasik@gmail.com>
2021-04-07 22:25:07 +1000

    Add: token preparation

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -16,6 +16,15 @@ RSpec.describe AccessToken, type: :model do
       access_token.token = 'token'
       expect(access_token).to be_valid
     end
+
+    it "should have a token present after initialize" do 
+      expect(AccessToken.new.token).to be_present
+    end
+
+    it "should generate uniq token" do
+      user = create :user
+      expect{ user.create_access_token }.to change{ AccessToken.count }.by(1)
+      expect(user.build_access_token).to be_valid
+    end
   end
-  
 end
