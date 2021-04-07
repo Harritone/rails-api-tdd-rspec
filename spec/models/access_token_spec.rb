e73c0ddf0568027ec74278ea7dca8132df2e2e5b
Hariton <yourrasik@gmail.com>
2021-04-07 22:16:17 +1000

    Add: token validation

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AccessToken, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#validation" do
+    it "should have valid factory" do
+      user = create :user
+      access_token = build :access_token, user_id: user.id
+      expect(access_token).to be_valid
+    end
+
+    it "should validate presence of token" do 
+      user = create :user
+      access_token = build :access_token, user_id: user.id
+      access_token.token = nil
+      expect(access_token).not_to be_valid
+      access_token.token = 'token'
+      expect(access_token).to be_valid
+    end
+  end
+  
 end
