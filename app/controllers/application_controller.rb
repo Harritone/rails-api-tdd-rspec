5c0ebeeb7cb97235769b2e2c3782f621ad3af621
Hariton <yourrasik@gmail.com>
2021-04-10 21:45:29 +1000

    Add: logout and authorize!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,27 @@
 class ApplicationController < ActionController::API
-  class AuthorizationError<StandardError; end
+
+  class AuthorizationError < StandardError; end
+
   rescue_from UserAuthenticator::AuthenticationError, with: :authenication_error
   rescue_from AuthorizationError, with: :authorization_error
 
+  before_action :authorize!
+
   private
+
+  def authorize!
+    raise AuthorizationError unless current_user
+  end
+
+  def acces_token
+    provided_token = request.authorization&.gsub(/\ABearer\s/, '')
+    @access_token = AccessToken.find_by(token: provided_token)
+  end
+
+  def current_user
+    @current_user = acces_token&.user
+  end
+
   def authenication_error
     error = {
       "status"=> "401",
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,14 @@
 class ArticlesController < ApplicationController
+  skip_before_action :authorize!, only: %i[index show]
+
   include Paginable
 
   def index
     paginated = paginate(Article.recent)
     render_collection(paginated)
+  end
+
+  def show
   end
 
   def serializer
