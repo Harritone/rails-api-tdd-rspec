870200b81af12ba29f5e362ea1502c6bbe133632
Hariton <yourrasik@gmail.com>
2021-04-10 16:39:15 +1000

    Add: registration and login processes

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -3,6 +3,11 @@ class AccessTokensController < ApplicationController
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
+
+    render json: serializer.new(authenticator.access_token), status: :created
   end
     
+  def serializer
+    AccessTokenSerializer
+  end
 end
--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController do 
   describe "#create" do 
-    context "when request is invalid" do
+
+    shared_examples_for "unauthorized_requests" do
       let(:error) do
         {
           "status": "401",
@@ -11,19 +12,73 @@ RSpec.describe AccessTokensController do
           "detail": "You must provide valid code in order to exchange it for token"
         }
       end
+
       it 'should return 401 status code' do
-        post :create
+        subject
         expect(response).to have_http_status(401)
       end
-
+      
       it 'should return propper error body' do
-        post :create
+        subject
         expect(json[:errors]).to include(error)
       end
     end
     
+    context "when no code provided" do
+      subject { post :create }
+      it_behaves_like "unauthorized_requests"
+    end
+
+    context "when invalid code provided" do
+      let(:github_error) {
+        double("Sawyer::Resource", error: "bad_verification_code")
+      }
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return(github_error)
+      end
+
+      subject { post :create, params: { code: 'invalid_code' } }
+
+      it_behaves_like "unauthorized_requests"
+    end
+    
     context "when success request" do
-      
+
+      subject { post :create, params: { code: 'valid_code' } }
+
+      let(:user_data) do
+        {
+          login: 'jsmith1',
+          url: 'http://example.com',
+          avatar_url: 'http://example.com/avatr',
+          name: 'John Smith'
+        }
+      end
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token
+        ).and_return('validaccesstoken')
+
+        allow_any_instance_of(Octokit::Client).to receive(
+          :user
+        ).and_return(user_data)
+      end
+
+      it 'should return 201 status code' do
+        subject 
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'should return proper json body' do
+        expect{ subject }.to change{ User.count }.by(1)
+        user = User.find_by(login: 'jsmith1')
+        expect(json_data[:attributes]).to eq(
+          { 'token': user.access_token.token }
+        )
+      end
     end
     
   end
