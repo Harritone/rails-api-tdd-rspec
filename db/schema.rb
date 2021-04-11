415edcad45bcbe28fbad4eb37fda4a4106dca03e
Hariton <yourrasik@gmail.com>
2021-04-11 11:17:35 +1000

    Add: user to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_115922) do
+ActiveRecord::Schema.define(version: 2021_04_11_011652) do
 
   create_table "access_tokens", force: :cascade do |t|
     t.string "token", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2021_04_07_115922) do
     t.string "slug"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2021_04_07_115922) do
   end
 
   add_foreign_key "access_tokens", "users"
+  add_foreign_key "articles", "users"
 end
