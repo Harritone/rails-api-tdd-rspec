ff4fa72d344ae8f1e840492feb8b05e5ca87b849
Hariton <yourrasik@gmail.com>
2021-04-14 13:19:39 +1000

    Add: implement registration using login and password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,14 @@
 class RegistrationsController < ApplicationController
+  skip_before_action :authorize!, only: :create
+  def create
+    user = User.new(registration_params.merge(provider: 'standard'))
+    user.save!
+    render json: user, status: :created
+  end
+
+  private 
+  def registration_params 
+    params.dig(:data, :attributes).permit(:login, :password) || 
+    ApplicationController::Parameters.new
+  end
 end
